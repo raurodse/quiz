3ced5799dbff25efbfd6cd19225470c7aaaf1644
raurodse <raurodse@gmail.com>
2015-07-09 19:18:59 +0200

    added author route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+router.get('/author',function(req,res,next){
+	res.render('author');
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
